b2f5e431c4ae6bd773bc6ff0bb7a6bc13ea9d03d
HearSilent <hear.silent1995@gmail.com>
2016-02-11 20:39:04 +0800

    Auto output JSON format, Update Sample I/O, Add 10402.json.

--- Calendar Crawler/Calendar Crawler/Form1.vb
+++ Calendar Crawler/Calendar Crawler/Form1.vb
@@ -1,4 +1,5 @@
 ﻿Imports HtmlAgilityPack
+Imports Newtonsoft.Json.Linq
 
 Public Class Form1
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
@@ -10,19 +11,32 @@ Public Class Form1
 
         Dim _table As HtmlNodeCollection = node.SelectNodes("//*[@id=""ContentPlaceHolder1_TabContainer1_TabPanelBsGov_div_ContentBsGov""]/table/tr")
 
+        Dim jsonArray As JArray = New JArray
+
         For i = 1 To _table.Count - 1
             Dim _scheduleTable As String = node.SelectNodes("//*[@id=""ContentPlaceHolder1_TabContainer1_TabPanelBsGov_TDContent_" & (i - 1) & "9""]")(0).InnerText
             If (Not _scheduleTable.Contains("(教)")) Then
                 Continue For
             End If
-            Debug.Print(node.SelectNodes("//*[@id=""ContentPlaceHolder1_TabContainer1_TabPanelBsGov_TDContent_" & (i - 1) & "1""]")(0).InnerText)
+
+            Dim _jsonObject As JObject = New JObject
+            Dim _jsonArray As JArray = New JArray
+
+            Dim _week As String = node.SelectNodes("//*[@id=""ContentPlaceHolder1_TabContainer1_TabPanelBsGov_TDContent_" & (i - 1) & "1""]")(0).InnerText
+            If Not (_week.Equals("寒") Or _week.Equals("暑") Or _week.Equals("預備週")) Then
+                _week = "第" + _week + "週"
+            End If
+
+            _jsonObject.Add(New JProperty("week", _week))
             For j = 1 To _scheduleTable.Split("*(教)").Count - 1
-                If (j = _scheduleTable.Split("*(教)").Count - 1) Then
-                    Debug.Print("""" + _scheduleTable.Split("*(教)")(j).Substring(3).Replace(")-", ") ") + """")
-                Else
-                    Debug.Print("""" + _scheduleTable.Split("*(教)")(j).Substring(3).Replace(")-", ") ") + """,")
-                End If
+                Dim _event As String = _scheduleTable.Split("*(教)")(j).Substring(3).Replace(")-", ") ")
+                Dim _splitIndex As Integer = _event.IndexOf(" ")
+                _event = _event.Substring(0, _splitIndex).Replace("-", " ~ ") + _event.Substring(_splitIndex)
+                _jsonArray.Add(_event)
             Next
+            _jsonObject.Add(New JProperty("events", _jsonArray))
+            jsonArray.Add(_jsonObject)
         Next
+        Debug.Print(jsonArray.ToString)
     End Sub
 End Class
--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # Calendar Crawler
 
 ## 使用方法：
-1. 自行至[行事曆](http://active.kuas.edu.tw/EPortfolio/Activity/UnitBsCalendar.aspx) 下載最新學期的行事曆原始碼，並將單位選擇為教務處
+1. 自行至 [行事曆](http://active.kuas.edu.tw/EPortfolio/Activity/UnitBsCalendar.aspx) 下載最新學期的行事曆原始碼，並將單位選擇為教務處
 2. 將原始碼貼上至程式 (需用Visual Studio開啟) 文字框，並按下按鈕
-3. 結果將顯示於除錯視窗，之後就工人智慧複製貼上，節省一點時間
+3. 結果將顯示於除錯視窗，之後將重複的 week 合併，以及沒完整顯示的 event 補齊，節省一點時間
 
 ## TODO List
-1. 自動轉JSON
-2. 輸出至檔案
+1. 輸出至檔案
+2. 自動抓取原始碼
